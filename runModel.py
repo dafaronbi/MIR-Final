256bcf4d43c89f9cab004bbf2a39f4a545c5269e
dafaronbi <dprdctions@gmail.com>
2021-12-19 14:25:28 -0500

    models trains with spectrogram reprdoction well

--- runModel.py
+++ runModel.py
@@ -15,14 +15,14 @@ test_data = ds.melParamData("test","data")
 validation_data = ds.melParamData("validation","data")
 
 #directory for finding checkpoints
-checkpoint_path = "saved_models/cp-{epoch:04d}.ckpt"
+checkpoint_path = "new_models4/cp-{epoch:04d}.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 #get latest model
 latest = tf.train.latest_checkpoint(checkpoint_dir)
 
 #create autoencoder model
-autoencoder = model.autoencoder(64,train_data.get_mels()[:10,...,np.newaxis].shape,train_data.get_params().shape)
+autoencoder = model.autoencoder3(64,train_data.get_mels()[:10,...,np.newaxis].shape,train_data.get_params().shape)
 
 #load stored weights
 autoencoder.load_weights(latest)
@@ -30,20 +30,29 @@ autoencoder.load_weights(latest)
 #compile model
 autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
 
-# #print evaluation on test set
-# loss, loss1,loss2 = autoencoder.evaluate(test_data.get_mels(),[test_data.get_mels(),test_data.get_params()])
-# print("model loss = " + str(loss) + "\n model spectrogram loss 1 = "+ str(loss1) + "\n model synth_param loss 2 = "+ str(loss2))
+#print evaluation on test set
+loss, loss1,loss2 = autoencoder.evaluate(train_data.get_mels(),[train_data.get_mels(),train_data.get_params()])
+print("training model loss = " + str(loss) + "\n training model spectrogram loss = "+ str(loss1) + "\n training model synth_param loss = "+ str(loss2))
+
+#print evaluation on test set
+loss, loss1,loss2 = autoencoder.evaluate(test_data.get_mels(),[test_data.get_mels(),test_data.get_params()])
+print("test model loss = " + str(loss) + "\n test model spectrogram loss = "+ str(loss1) + "\n test model synth_param loss = "+ str(loss2))
 
 #get prediction
-spectogram,params = autoencoder.predict(test_data.get_mels()[[20]])
+spectogram,params = autoencoder.predict(train_data.get_mels()[[30]])
 
 #evaluate reconstruction of 30th test file
 fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
-img = librosa.display.specshow(test_data.get_mels()[20], y_axis='mel', x_axis='time', ax=ax[0])
+img = librosa.display.specshow(train_data.get_mels()[30], y_axis='mel', x_axis='time', ax=ax[0])
 
 ax[0].set(title='Mel-Frequency Spectrogram Reconstruction')
 ax[0].label_outer()
 
 librosa.display.specshow(np.squeeze(spectogram), y_axis='mel', x_axis='time', ax=ax[1])
 fig.colorbar(img, ax=ax, format="%+2.f dB")
+
+
+print("Ground Truth Parameters:" + str(test_data.get_params()[500]-params))
+# print("Predicted Parameters" + str(params))
+
 plt.show()
--- train.py
+++ train.py
@@ -13,18 +13,27 @@ validation_data = ds.melParamData("validation","data")
 # Include the epoch in the file name (uses `str.format`)
 checkpoint_path = "saved_models/cp-{epoch:04d}.ckpt"
 
+#epoch size
+epochs=500
+
 #batch_size
-batch_size = 32
+batch_size = 8
+
+#number of batches in one epoch
+batches_epoch = ds.melParamData("train","data").get_mels().shape[0] // batch_size
+
+#save freq is every 10 epochs
+save_freq = batches_epoch*10
 
 # Create a callback that saves the model's weights every 50 epochs
 cp_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_path,
     verbose=1,
     save_weights_only=True,
-    save_freq=batch_size*10)
+    save_freq=save_freq)
 
 #create auto encoder model
-autoencoder = model.autoencoder(64,train_data.get_mels()[...,np.newaxis].shape,train_data.get_params().shape)
+autoencoder = model.autoencoder3(64,train_data.get_mels()[...,np.newaxis].shape,train_data.get_params().shape)
 
 #view summary of model
 autoencoder.summary()
@@ -36,8 +45,8 @@ autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
 autoencoder.optimizer.lr.assign(1e-3)
 
 #train model
-autoencoder.fit(train_data.get_mels()[...,np.newaxis],[train_data.get_mels(),train_data.get_params()], epochs=500, batch_size=batch_size, callbacks=[cp_callback])
+autoencoder.fit(train_data.get_mels()[...,np.newaxis],[train_data.get_mels(),train_data.get_params()], epochs=epochs, batch_size=batch_size, callbacks=[cp_callback])
 
 #print evaluation on test set
 loss, loss1,loss2 = autoencoder.evaluate(test_data.get_mels(),[test_data.get_mels(),test_data.get_params()],2)
-print("model loss = " + str(loss) + "\n model spectrogram loss 1 = "+ str(loss1) + "\n model synth_param loss 2 = "+ str(loss2))
+print("model loss = " + str(loss) + "\n model spectrogram loss = "+ str(loss1) + "\n model synth_param loss = "+ str(loss2))
